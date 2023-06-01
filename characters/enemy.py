50b9eba6b55b6bfa0af94c105f5db5a60b95bb42
Pedro Henrique Chagas <ph_chagas@id.uff.br>
2023-06-01 20:28:15 -0300

    novos assets; novas classes

---
+++ characters/enemy.py
@@ -0,0 +1,8 @@
+from vendor.pplay.window import *
+from vendor.pplay.sprite import *
+from vendor.pplay.gameimage import *
+from vendor.pplay.collision import *
+from vendor.pplay.animation import *
+
+#classe inimigo, classe mãe, cada inimigo vai herdar dela
+#acredito que só necessitaremos desses imports (bem possivel gameimage e sprite serem substituidos pelos respectivos resolve)
